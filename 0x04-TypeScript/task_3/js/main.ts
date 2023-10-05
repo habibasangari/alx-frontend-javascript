c4e276f4a792aa39cbffa877a3c417a65746eca0
habibasangari <55847759+habibasangari@users.noreply.github.com>
2023-10-05 01:28:58 +0100

    learning never ends

---
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -0,0 +1,7 @@
+/// <reference types='crud' />
+import { RowID, RowElement } from './interface.ts';
+import * as CRUD from './crud.js';
+
+const row: RowElement {
+  firstName: Guillaume,
+  lastName: Salva.
